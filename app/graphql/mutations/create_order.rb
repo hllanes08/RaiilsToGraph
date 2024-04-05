9912a2e31efbf4c26ff0e417e3cd84b2414e169b
Harvin LLanes <harvin.llanes@gmail.com>
2024-04-05 16:36:15 -0600

    feat(Mutation/Order/Create) Add mutation for create a new order

--- app/graphql/mutations/create_order.rb
+++ app/graphql/mutations/create_order.rb
@@ -1,10 +1,21 @@
 module Mutations
   class CreateOrder < BaseMutation
-    field :article_id, Integer
-    field :total, Float
-    field :quantity, Integer
+    argument :article_id, Integer, required: true
+    argument :total, Float, required: true
+    argument :quantity, Integer, required: true
+    
+    field :order, Types::OrderType#, null: true
+    field :errors, [String]#, null: false
 
-    def resolve(total: , quantity: , :article_id)
+    def resolve(total: , quantity: , article_id:)
+      if context[:current_user].present? 
+        order = Order.new(quantity: quantity, article_id: article_id, total: total)
+        order.user_id = context[:current_user].id
+        order.save!
+         { order: order, errors: [] }
+      else
+        { order: nil, errors: ['Unauthorized'] }
+      end
     end
   end
 end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,6 +2,7 @@
 
 module Types
   class MutationType < Types::BaseObject
-     field :login, mutation: Mutations::UserLogin 
+     field :login, mutation: Mutations::UserLogin
+     field :create_order, mutation: Mutations::CreateOrder 
   end
 end
